52f4b5c8ab205a6e82679312f3663864e1181a1c
cheeseconnoisseur <arturo.dezon@gmail.com>
2018-01-02 16:32:53 +0000

    I DID IT I FOOKIN DID TI FINALLY OMG I WAS ABOUT TO NAG MYSELF PREPROCCESING IS BASICLLY DONE OMG I BELIEVE IN GOD AGAIN

--- regression1/regression.py
+++ regression1/regression.py
@@ -2,6 +2,7 @@ import pandas as pd
 import quandl
 import math
 import datetime as dt
+from datetime import datetime
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
@@ -19,8 +20,8 @@ quandl.ApiConfig.api_key = '2PDeyG166PJWMeZ1fZqi'
 ##    print(dp)
 ##    fday = n.ix[0]['Date']
 ##    fday = pd.Series(n)
-##    
-##    
+##
+##
 ##    print(n.head())
 ##    print(' ')
 ##    print(len(n))
@@ -39,14 +40,16 @@ def getstock():
     print(data)
     print(data.head())
     dp = input('how many days in advanced to predict:')
+    dp = int(dp)
     print(dp)
 
     data.to_csv('data.csv')
     datacv = pd.read_csv('data.csv')
     fday = datacv.iloc[-1,0]
+    lday = datacv.iloc[0,0]
 
 
-    
+
     print(data.head())
     print(' ')
     print(len(data))
@@ -55,11 +58,34 @@ def getstock():
     print(' ')
 
     print(fday)
+    fday = str(fday)
+    lday = str(lday)
+    print(fday)
+    fday = fday.replace("-", "")
+    lday = lday.replace("-", "")
+    
+    print(fday) 
+    fday = datetime.strptime(fday,'%Y%m%d')
+    lday = datetime.strptime(lday,'%Y%m%d')
+    print('###########')
+    print(fday)
+    print(lday)
+    print('###########')
+
     #iloc (pandas) finds something by index or position
-    last_unix = pd.to_datetime('13000101', format='%Y%m%d', errors='ignore',infer_datetime_format=True)
+##    fday =  pd.to_datetime(fday, format='%Y%m%d', errors='ignore',infer_datetime_format=True)
+##    lday =  pd.to_datetime(lday, format='%Y%m%d', errors='ignore',infer_datetime_format=True)
     oneday = 60*60*24 #(86400)
-    next_unix = last_unix + dt.timedelta(minutes=1440)
-    print(next_unix)
+    ftlday = fday - lday
+    ftlday = str(ftlday)
+    print(ftlday)
+    ftlday = ftlday.replace(" days, 0:00:00", "")
+    ftlday = int(ftlday)
+    print(ftlday)
+    forday = (dp/ftlday)
+    print(forday)
+    forrday = forday*100
+    print(forrday)
 
 
 
@@ -80,10 +106,10 @@ def getforex():
 
 def first():
    # try:
- #       pickle_in = open('linearregression.pickle','rb')
-  #  except:
+   #     pickle_in = open('linearregression.pickle','rb')
+   #  except:
     pik = 1
-    choice = input('stock or forex')
+    choice = input('stock or forex(in beta)')
     if choice == 'stock' or 's':
         getstock()
     elif choice == 'f' or 'forex':
