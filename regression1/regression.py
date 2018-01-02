1355306589d736770903c2b1a0b9422695580a6b
cheeseconnoisseur <arturo.dezon@gmail.com>
2018-01-02 14:15:28 +0000

    succ

--- regression1/regression.py
+++ regression1/regression.py
@@ -11,22 +11,58 @@ import pickle
 
 pik = 0
 data = []
+quandl.ApiConfig.api_key = '2PDeyG166PJWMeZ1fZqi'
 
+
+##def process(n):
+##    dp = input('how many days in advanced to predict:')
+##    print(dp)
+##    fday = n.ix[0]['Date']
+##    fday = pd.Series(n)
+##    
+##    
+##    print(n.head())
+##    print(' ')
+##    print(len(n))
+##    print(fday)
 
 
 def getstock():
-    stock = input('ENTER STOCK')
-    print(stock)
-    time1 = input('from date:yyyy-mm-dd')
-    print(time1)
-    time2 = input('to date:yyyy-mm-dd')
-    print(time2)
-    try:
-        quandl.ApiConfig.api_key = '2PDeyG166PJWMeZ1fZqi'
-        data = quandl.get("WIKI/"+stock, start_date="2001-12-31", end_date="2005-12-31")
-        print(data)
-    except:
-        print('no')
+    #stock = input('ENTER STOCK')
+    #print(stock)
+    #time1 = input('from date:yyyy-mm-dd')
+    #print(time1)
+    #time2 = input('to date:yyyy-mm-dd')
+    #print(time2)
+    quandl.ApiConfig.api_key = '2PDeyG166PJWMeZ1fZqi'
+    data = quandl.get("WIKI/GOOGL", start_date="2001-12-31", end_date="2005-12-31")
+    print(data)
+    print(data.head())
+    dp = input('how many days in advanced to predict:')
+    print(dp)
+
+    data.to_csv('data.csv')
+    datacv = pd.read_csv('data.csv')
+    fday = datacv.loc[-1,0]
+
+
+    
+    print(data.head())
+    print(' ')
+    print(len(data))
+    print(' ')
+    print(' ')
+    print(' ')
+
+    print(fday)
+##    #iloc (pandas) finds something by index or position
+##    last_unix = fday.timestamp()
+##    oneday = 60*60*24 #(86400)
+##    next_unix = last_unix + oneday
+##    print(next_unix)
+
+
+
 
 def getforex():
     forr = input('ENTER FOREX')
@@ -36,21 +72,23 @@ def getforex():
     time2 = input('to date:yyyy-mm-dd')
     print(time2)
     try:
-        quandl.ApiConfig.api_key = '2PDeyG166PJWMeZ1fZqi'
+
         data = quandl.get("FRED/"+forr, start_date="2001-12-31", end_date="2005-12-31")
         print(data)
     except:
         print('no')
 
 def first():
-    try:
-        pickle_in = open('linearregression.pickle','rb')
-    except:
-        pik = 1
-        choice = input('stock or forex')
-        if choice == 'stock' or 's':
-            getstock()
-        else:
-            getforex()
-    print('lol')
+   # try:
+ #       pickle_in = open('linearregression.pickle','rb')
+  #  except:
+    pik = 1
+    choice = input('stock or forex')
+    if choice == 'stock' or 's':
+        getstock()
+    elif choice == 'f' or 'forex':
+        getforex()
+    process(data)
+
+
 first()
