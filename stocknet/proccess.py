67dd3351a5a42f20317d465cd8a03106c6814bde
cheeseconnoisseur <arturo.dezon@gmail.com>
2018-01-04 22:23:20 +0000

    ive gotten pretty far in the process i need to fix the csv problem and add an extra counter for the y testing data but i can get there il also make a small termianl thing to find stocks

--- stocknet/proccess.py
+++ stocknet/proccess.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import pickle
 from dateutil.relativedelta import relativedelta
+import numpy as np
+#import csv
 
 quandl.ApiConfig.api_key = '2PDeyG166PJWMeZ1fZqi'
 style.use('ggplot')
@@ -19,7 +21,7 @@ style.use('ggplot')
     data = quandl.get("WIKI/"+stock, start_date=time1, end_date=time2)
 '''
 print("mkay")
-time1 = '2001-01-01'
+time1 = '2004-09-16'
 time2 = '2018-01-01'
 
 #time1 = time1.replace("-", "")
@@ -50,17 +52,48 @@ data = quandl.get("WIKI/GOOGL", start_date='2001-01-01', end_date='2018-01-01')
 print (data)
 data['PCT_change'] = (data['Adj. Close'] - data['Adj. Open']) / data['Adj. Open'] * 100
 data = data[['Adj. Close','PCT_change','Adj. Volume']]
-
-def changetoform(yeet):
-    yeet = yeet.date()
-    yeet.strftime('%m/%d/%Y')
-    print (yeet)
-
+bigarray = np.array([])
+def changetoform(df1):
+    df1 = df1.date()
+    df1.strftime('%Y-%m-%d')
+    df1 = str(df1)
+    df1 = df1.replace("-", "")
+    return(df1)
 
 while time1 not in [time2,time2s1,time2s2,time2s3,time2s4,time2s5,time2s6,time2s7,time2s9,time2s10]:
-    time1 = time1 + dt.timedelta(days=1)#relativedelta(day=1)
-    df1=data.loc[time1]
-    print(df1)
+    time1 = time1 + dt.timedelta(days=1) #dt.timedelta(days=1)#df1 = changetoform(df1)df1=data.loc[[df1]]print(df1)
+    df1=time1
+    df1 = changetoform(df1)
+    try:
+        df1=data.loc[df1,'PCT_change']
+        print(df1)
+        print(time1)
+
+
+        if df1 < -0.5:
+            a = np.array([0,0,1])
+            bigarray = np.append(bigarray,a)
+            print(bigarray)
+        elif df1 -0.5 < df1 < 0.5:
+            b = np.array([0,1,0])
+            bigarray = np.append(bigarray,b)
+            print(bigarray)
+        elif 0.5 < df1:
+            c = np.array([1,0,0])
+            bigarray = np.append(bigarray,c)
+            print(bigarray)
+
+            if bigarray.size >=30:
+                print(bigarray)
+                with open('data.csv','rb') as file:
+                    file.write(bigarray)
+                    bigarray = np.array([])
+                    file.close()
+    except:
+        print("skipped")
+    continue
+
+
 print("done")
 '''
 #forecast_col = 'Adj. Close'
--- stocknet/test.py
+++ stocknet/test.py
@@ -5,12 +5,40 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import pickle
 from dateutil.relativedelta import relativedelta
-
+import numpy as np
+'''
 data = quandl.get("WIKI/GOOGL", start_date='2001-01-01', end_date='2018-01-01')
 print (data)
 data['PCT_change'] = (data['Adj. Close'] - data['Adj. Open']) / data['Adj. Open'] * 100
 data = data[['Adj. Close','PCT_change','Adj. Volume']]
 
-df1=data.loc["20040917"]
+df1=data.loc["20040917",'PCT_change']
+df2=data.loc["20040920",'PCT_change']
+df3=data.loc["20040921",'PCT_change']
 print("done")
-print(df1)
+print(df1,df2,df3)
+'''
+'''
+time1 = '2001-01-01'
+time2 = '2018-01-01'
+
+#time1 = time1.replace("-", "")
+#time1 = time1.replace("-", "")
+
+time1 = dt.datetime.strptime(time1, "%Y-%m-%d")
+time2 = dt.datetime.strptime(time2, "%Y-%m-%d")
+'''
+'''
+yeet = time1
+yeet = yeet.date()
+yeet.strftime('%Y-%m-%d')
+yeet = str(yeet)
+yeet = yeet.replace("-", "")
+print(yeet)
+'''
+
+
+a = np.array([0,1,0])
+b = np.array([])
+c = np.append(a,b)
+print(c)
